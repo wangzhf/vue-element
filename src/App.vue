8d8c46dd03b47edb59921a263cea741bc41acaa8
wangzhf <zhifengtech@gmail.com>
2018-07-29 22:42:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped >
+#app {
+    text-align: center;
+}
+</style>
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import router from './router'
+import Element from 'element-ui'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(Element, {size: 'medium', zIndex: 3000})
+
+new Vue({
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App />'
+});
